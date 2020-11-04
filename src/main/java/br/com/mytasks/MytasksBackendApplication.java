047d3cd6a6dee5648da35b3a2dfb86ab9f39b794
Daniel Petrico <daniel@danielpetrico.com>
2020-11-04 20:19:27 -0300

    criacao do projeto spring-boot

---
+++ src/main/java/br/com/mytasks/MytasksBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.mytasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytasksBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytasksBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mytasks/MytasksBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mytasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytasksBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
